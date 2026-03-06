cafc21210b208891e6f6263e8b72479f7f9e3e57
simz1987 <simz1987@gmail.com>
2026-03-06 08:10:55 +0000

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Upload your Summary report. The app will automatically translate the c
 # 🛑 YOUR PRODUCT DICTIONARY 🛑
 # Replace the lines below with your giant generated list!
 # ==========================================
-   "1841": "184105",
+    "1841": "184105",
     "2624": "",
     "2625": "",
     "2627": "",
@@ -564,3 +564,4 @@ if summary_file:
 
         except Exception as e:
             st.error(f"An unexpected error occurred: {e}")
+
